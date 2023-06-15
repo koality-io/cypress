97be34a47cdbb66bc3fc4df069049121e01431d3
Nils Langner <nils.langner@phmlabs.com>
2023-06-15 19:35:31 +0200

    some fixes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,19 @@
 describe('login', () => {
-  it('login as user', () => {
+    it('login as user', () => {
 
-    const startUrl = Cypress.config().baseUrl;
+        const startUrl = Cypress.config().baseUrl;
 
-    cy.visit(startUrl)
+        cy.visit(startUrl)
 
-    cy.fixture('user').then((user) => {
-      cy.get('#username').type(user.pro.username)
-      cy.get('#password').type(user.pro.password)
+        cy.fixture('user').then((user) => {
+            cy.get('#username').type(user.pro.username)
+            cy.get('#password').type(user.pro.password)
+
+            cy.get('a[data-cy="trigger-login"]').click()
+
+            cy.contains('Create new project')
+            cy.contains('www.koality.io (europe)')
+        })
+
     })
-
-    cy.get('a[data-cy="trigger-login"]').click()
-
-    // cy.contains('Create new project')
-    cy.contains('www.koality.io (europe)')
-  })
 })
